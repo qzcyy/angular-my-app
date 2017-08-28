36c251327985025cbb284878fbceb11dc2e97a6d
niuhaitao5 <Ppgs8903!>
2017-08-28 18:29:04 +0800

    rx-h2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {Component} from "@angular/core"
 @Component({
   selector:"my-app",
   template:`<h1>{{title}}</h1>
+  <h2>123</h2>
     <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
     <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
    <router-outlet></router-outlet>`
